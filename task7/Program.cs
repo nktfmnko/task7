6252c909c4b6437f0694bd5e1cd23c485cc239ce
nktfmnko <nktfmnko@gmail.com>
2023-12-12 15:40:02 +0300

    Добавьте файлы проекта.

---
+++ task7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double MultiplyBy2(double x)
+            {
+                return x * 2;
+            }
+
+            Console.WriteLine(MultiplyBy2(4));
+
+        }
+    }
+}

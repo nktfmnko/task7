b95b5fb8b6b23c9befccc1da8bd8fb4776fab252
nktfmnko <nktfmnko@gmail.com>
2023-12-12 17:58:46 +0300

    try add to yandex tracker

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace task7
 {
@@ -14,9 +17,40 @@ namespace task7
         }
         static void Main(string[] args)
         {
-         
-            Console.WriteLine(MultiplyBy2(4));
 
+            
+            string token = "y0_AgAAAABpte-yAATuwQAAAAD0l16X-a2qI29xSGCi8ROP9yidJ0Wk1v0";
+
+            
+            using (var client = new HttpClient())
+            {
+            
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", token);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            
+                var taskData = new
+                {
+                    queue_id = "TEAMCITYBUILDFA",
+                    summary = "Build failed",
+                    description = "Build failed in TeamCity",
+                    assignee = "nktfmnko@gmail.com"
+                };
+
+            
+                var response = client.PostAsync("https://api.tracker.yandex.net/v2/issues/", new StringContent(JsonConvert.SerializeObject(taskData), Encoding.UTF8, "application/json")).Result;
+
+            
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Task created successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Error creating task: " + response.StatusCode);
+                }
+                Console.WriteLine(MultiplyBy2(4));
+            }
         }
     }
 }
